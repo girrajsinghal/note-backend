1d9d7bd063e12cd98f9764e6570f8ccce8b3181b
Girraj singhal <girrajsinghal48@gmail.com>
2024-08-25 22:21:12 +0530

    Add files via upload

---
+++ src/components/CreateNote.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const CreateNote = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate();
+
+  const createNote = async () => {
+    try {
+      await axios.post("http://localhost:5000/api/createnote", {
+        title,
+        content,
+      });
+      navigate("/"); // Navigate to the page displaying all notes
+    } catch (error) {
+      console.error("Error creating note:", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Create Note</h1>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button className="create" onClick={createNote}>
+        Create
+      </button>
+    </div>
+  );
+};
+
+export default CreateNote;
